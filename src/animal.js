063df6d7f1aa82dedae222fe04f3a2cb6712c48b
nandiKhumalo30 <nandi.khumalo@umuzi.org>
2021-02-04 14:48:24 +0200

    Added tests to the animal oop project

---
+++ src/animal.js
@@ -0,0 +1,39 @@
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+    eat() {
+      return "Food";
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name) {
+      super(name);
+    }
+    sound() {
+      return "Bark";
+    }
+  }
+  
+  class Cat extends Animal {
+    constructor(name) {
+      super(name);
+    }
+    sound() {
+      return "Meow";
+    }
+  }
+  
+  class Home {
+    adoptPet(pet) {
+      adoptedPets.push(pet);
+    }
+    makeAllSounds() {
+      let adoptedPets = [];
+      for (var i = 0; i < adoptedPets.length; i++) {
+        adoptedPets[i].sound();
+      }
+    }
+  }
+  module.exports = { Dog, Cat };
