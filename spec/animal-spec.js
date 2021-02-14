3a16f287fec72b8b20c9dbcabe54b4abc5eb84c4
nandiKhumalo30 <nandi.khumalo@umuzi.org>
2021-02-14 23:16:23 +0200

    refracted code and added more test

--- spec/animal-spec.js
+++ spec/animal-spec.js
@@ -1,25 +1,46 @@
-const { Dog, Cat} = require("../src/animal.js");
+const { Dog, Cat, Home} = require("../src/animal.js");
 var dog = new Dog();
 var cat = new Cat();
+var ds = dog.sound()
+var cs = cat.sound()
+var home = new Home();
+var adoptDog = home.adoptPet(dog);
+var dogSound = home.makeAllSounds();
+var adoptCat = home.adoptPet(cat);
+var catSound = home.makeAllSounds();
 
-describe("Dog", function () {
-  it("should expect Dog to eat Food", function () {
+describe("Dog", () => {
+  it("should expect Dog to eat Food", () => {
     expect(dog.eat()).toBe("Food");
     expect(dog.eat()).not.toBe("food");
   });
-  it("should expect Dog sound to be Bark", function () {
+  it("should expect Dog sound to be Bark", () => {
     expect(dog.sound()).toBe("Bark");
   });
 });
 
-describe("Cat", function () {
-  it("should expect Cat to eat Food", function () {
+describe("Cat", () => {
+  it("should expect Cat to eat Food", () => {
     expect(cat.eat()).toBe("Food");
     expect(cat.eat()).not.toBe("food");
     expect(cat.eat()).not.toBe("meat");
   });
-  it("should expect Cat sound to be Meow", function () {
+  it("should expect Cat sound to be Meow", () => {
     expect(cat.sound()).toBe("Meow");
     expect(cat.sound()).not.toBe("Barkark");
   });
+})
+
+describe("Home", () => {
+  it("should expect pets to be adopted", () => {
+    expect(adoptDog).toBe("Dog");
+    expect(adoptCat).toBe("Cat");
+  })
+  it("should expect adopted pet dog sound to be Bark", () => {
+    expect(dogSound).toBe("Bark")
+  })
+  it("should expect adopted pet cat sound to be Meow", () => {
+    expect(catSound).toBe("Meow")
+  })
+  
 })
--- src/animal.js
+++ src/animal.js
@@ -1,39 +1,51 @@
 class Animal {
-    constructor(name) {
-      this.name = name;
-    }
-    eat() {
-      return "Food";
+  constructor(name) {
+    this.name = name;
+  }
+  eat() {
+    return "Food";
+  }
+}
+
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  sound() {
+    return "Bark";
+  }
+}
+
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+  }
+  sound() {
+    return "Meow";
+  }
+}
+var dog = new Dog('Rax');
+var cat = new Cat('Stormy');
+var ds = dog.sound()
+var cs = cat.sound()
+var adoptedPets = []
+
+class Home {
+  adoptPet(pet) {
+    console.log(adoptedPets.push(pet));
+  }
+  makeAllSounds() {
+    for (let i = 0; i < adoptedPets.length; i++) {
+      console.log(adoptedPets[i].sound());
     }
   }
-  
-  class Dog extends Animal {
-    constructor(name) {
-      super(name);
-    }
-    sound() {
-      return "Bark";
-    }
-  }
-  
-  class Cat extends Animal {
-    constructor(name) {
-      super(name);
-    }
-    sound() {
-      return "Meow";
-    }
-  }
-  
-  class Home {
-    adoptPet(pet) {
-      adoptedPets.push(pet);
-    }
-    makeAllSounds() {
-      let adoptedPets = [];
-      for (var i = 0; i < adoptedPets.length; i++) {
-        adoptedPets[i].sound();
-      }
-    }
-  }
-  module.exports = { Dog, Cat };
+}
+
+var home = new Home();
+var adoptDog = home.adoptPet(dog);
+var dogSound = home.makeAllSounds();
+var adoptCat = home.adoptPet(cat);
+var catSound = home.makeAllSounds();
+console.log()
+
+module.exports = { Dog, Cat, Home };
